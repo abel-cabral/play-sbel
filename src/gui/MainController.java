3e072f20bc0657f474a8dda08860a4a6fa14a9f4
abel-cabral <jcabral@id.uff.br>
2019-08-09 03:00:44 -0300

    Decrementador de tempo

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -9,6 +9,7 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayer.Status;
 import javafx.scene.media.MediaView;
+import util.TimeConvert;
 
 import java.io.File;
 import java.net.URL;
@@ -20,7 +21,7 @@ import java.util.TimerTask;
 public class MainController implements Initializable {
     SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
 
-    Timer timer = new Timer();
+
     Double timeMusic;
 
 
@@ -62,9 +63,15 @@ public class MainController implements Initializable {
         mediaPlayer.setOnReady(new Runnable() {
             @Override
             public void run() {
-                mediaPlayer.play();
                 timeMusic = mediaPlayer.getTotalDuration().toMillis();
+
+                // Inicialização de componentes de tela
+                timeScreen.setText(TimeConvert.convertToMinute(timeMusic, "mm:ss"));
+                musicName.setText(new File(source).getName().toUpperCase());
                 timeBar(mediaPlayer.getTotalDuration().toMillis());
+                timeLabel();
+
+                mediaPlayer.play();
             }
         });
 
@@ -77,26 +84,44 @@ public class MainController implements Initializable {
 
 
     private void initMusicPlayer(String source) {
-        musicName.setText(new File(source).getName().toUpperCase());
         media = new Media(new File(source).getPath());
 
         // Instantiating MediaPlayer class
         mediaPlayer = new MediaPlayer(media);
         mediaView.setMediaPlayer(mediaPlayer);
 
+        // Inicialização de componentes de tela
+
+
         mediaPlayer.setVolume(80);
         progressBar.setValue(0);
     }
 
     private void timeBar(double millis) {
+        Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             public void run() {
                 progressBar.setValue(progressBar.getValue() + 0.1);
-                if (progressBar.getValue() >= 100 || mediaPlayer.getStatus().PAUSED == Status.PAUSED) {
-                    System.out.println("Fim de timeBar");
+                if (progressBar.getValue() >= 100 || mediaPlayer.getStatus() == Status.PAUSED) {
+                    progressBar.setValue(0);
                     timer.cancel();
                 }
             }
         }, 0, (int) Math.round(millis) / 1000);
+    }
+
+    private void timeLabel() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                Platform.runLater(() -> timeScreen.setText(TimeConvert.convertToMinute(timeMusic, "mm:ss")));
+                timeMusic -= 1000;
+
+                if (timeMusic <= 0 || mediaPlayer.getStatus() == Status.PAUSED) {
+                    timeScreen.setText("");
+                    timer.cancel();
+                }
+            }
+        }, 1000, 1000);
     }
 }
